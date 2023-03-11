59a4340f43b4800997d82a7eb7791978f911f8b2
yukoono <unison.299792@gmail.com>
2023-03-12 00:15:38 +0900

    feat: ログイン処理で失敗するケースのflashメッセージ出力とテストを記載

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprecessable_entity
+    end
+  end
 end
