a2044a8b64278c13085eeaed9b95cc98f02e08e7
yukoono <unison.299792@gmail.com>
2023-03-12 15:14:30 +0900

    feat: ログインしたユーザーのIDを session と current_user に保存する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprecessable_entity
